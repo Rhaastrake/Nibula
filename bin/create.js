bb9aa8d8f7ee79e3cd88ebf5d61bf137732aa4f1
Rhaastrake <garofalomichele2001@gmail.com>
2026-05-06 22:28:11 +0200

    Changed npmjs ver

--- bin/create.js
+++ bin/create.js
@@ -17,7 +17,7 @@ const COPY_TARGETS = [
 
 const PROJECT_PACKAGE = {
     name: path.basename(targetDir),
-    version: '1.0.19',
+    version: '1.0.20',
     private: true,
     scripts: {
         'build:css': 'sass src/scss:out/css --no-source-map --style=compressed --quiet',
